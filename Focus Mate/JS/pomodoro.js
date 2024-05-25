6e59b949f188b91808be643656686d2896596364
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-05-25 09:44:40 +0530

    Focus Mate extension is added

---
+++ Focus Mate/JS/pomodoro.js
@@ -0,0 +1,98 @@
+// Define variables for timer state
+let progressInterval;
+let timerValue;
+let pomodoroType;
+let multiplierFactor;
+let audio = new Audio('../assets/audio/alarm.mp3');
+
+// Define constants for timer values
+const pomodoroTimerInSeconds = 1500; // 60 seconds * 25 minutes
+const shortBreakTimerInSeconds = 300; // 60 seconds * 5 minutes
+const TIMER_TYPE_POMODORO = 'POMODORO';
+const TIMER_TYPE_SHORT_BREAK = 'SHORTBREAK';
+
+// Retrieve timer value from local storage when extension is launched
+chrome.runtime.onStartup.addListener(() => {
+    chrome.storage.local.get(["timerValue"], (result) => {
+        timerValue = result.timerValue || pomodoroTimerInSeconds;
+    });
+});
+
+
+document.getElementById("start-btn").addEventListener("click", function(){
+    startTimer();
+});
+
+/* Start Timer */
+const startTimer = () => {
+    console.log("start");
+    
+    progressInterval = setInterval(() => {
+        if (localStorage.getItem("timer-value") === 0){
+            stopTimer();
+        }
+            
+        timerValue--;
+        setInfoCircularProgressBar();
+        // Update local storage with the updated timer value
+        chrome.storage.local.set({ "timerValue": timerValue });
+    }, 1000);
+}
+
+/* Stop Timer */
+const stopTimer = () => {
+    clearInterval(progressInterval);
+}
+
+/* Reset Timer */
+const resetTimer = () => {
+    clearInterval(progressInterval);
+    timerValue = (pomodoroType === TIMER_TYPE_POMODORO) ? pomodoroTimerInSeconds : shortBreakTimerInSeconds;
+    
+    multiplierFactor = 360 / timerValue;
+    setInfoCircularProgressBar();
+    audio.pause();
+    // Update local storage with the reset timer value
+    localStorage.setItem("timer-value", timerValue);
+}
+
+/* update the circular progress bar */
+function setInfoCircularProgressBar() {
+    if (timerValue === 0) {
+        stopTimer();
+        audio.play();
+    }
+    // Update circular progress bar
+    let circularProgressBar = document.querySelector('#circularProgressBar');
+    let circularProgressBarNumber = document.querySelector('#circularProgressBar .progress-value');
+    circularProgressBarNumber.textContent = `${formatNumberInStringMinutes(timerValue)}`;
+    circularProgressBar.style.background = `conic-gradient( ${timerValue * multiplierFactor } 360deg, purple 0deg)`;
+}
+
+/* format timer value in minutes:seconds */
+function formatNumberInStringMinutes(number) {
+    const minutes = Math.trunc(number / 60).toString().padStart(2, '0');
+    const seconds = Math.trunc(number % 60).toString().padStart(2, '0');
+    return `${minutes}:${seconds}`;
+}
+
+/* handle setting pomodoro type */
+const setPomodoroType = (type) => {
+    pomodoroType = type;
+    let buttonTypePomodoro = document.querySelector('#buttonTypePomodoro');
+    let buttonTypeShortBreak = document.querySelector('#buttonTypeShortBreak');
+    if (type === TIMER_TYPE_POMODORO) {
+        buttonTypeShortBreak.classList.remove("active");
+        buttonTypePomodoro.classList.add("active");
+    } else {
+        buttonTypePomodoro.classList.remove("active");
+        buttonTypeShortBreak.classList.add("active");
+    }
+    resetTimer();
+};
+
+// Event listeners for button clicks to start, stop, reset timers
+document.getElementById("stop-btn").addEventListener("click", stopTimer);
+document.getElementById("reset-btn").addEventListener("click", resetTimer);
+document.getElementById("buttonTypePomodoro").addEventListener("click", () => setPomodoroType(TIMER_TYPE_POMODORO));
+document.getElementById("buttonTypeShortBreak").addEventListener("click", () => setPomodoroType(TIMER_TYPE_SHORT_BREAK));
