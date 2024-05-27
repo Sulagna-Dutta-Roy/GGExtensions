c06d8dad7423599ab0831d85039055680529ca34
Yashgabani845 <gabaniyash845@gmail.com>
2024-05-27 22:23:47 +0530

    my own api key added

--- Currency Converter/js/script.js
+++ Currency Converter/js/script.js
@@ -56,7 +56,7 @@ function getExchangeRate(){
         amountVal = 1;
     }
     exchangeRateTxt.innerText = "Getting exchange rate...";
-    let url = `https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/${fromCurrency.value}`;
+    let url = `https://v6.exchangerate-api.com/v6/d9a72a90e9c9ae471a856f67/latest/${fromCurrency.value}`;
     // fetching api response and returning it with parsing into js obj and in another then method receiving that obj
     fetch(url).then(response => response.json()).then(result =>{
         let exchangeRate = result.conversion_rates[toCurrency.value]; // getting user selected TO currency rate
