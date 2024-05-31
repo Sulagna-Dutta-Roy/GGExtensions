5674a2e0628ffde1e8433ed0cff6f1dd449e74e8
unknown <bhumikaa275@gmail.com>
2024-06-01 02:39:42 +0530

    determinant calculator

---
+++ Determinant Calculator/script.js
@@ -0,0 +1,66 @@
+function generateMatrixTable() {
+    var size = parseInt(document.getElementById("sizeInput").value);
+    var matrixTableDiv = document.getElementById("matrixTable");
+    matrixTableDiv.innerHTML = "";
+
+    var table = document.createElement("table");
+    table.id = "matrixInputTable";
+
+    for (var i = 0; i < size; i++) {
+        var row = document.createElement("tr");
+        for (var j = 0; j < size; j++) {
+            var cell = document.createElement("td");
+            var input = document.createElement("input");
+            input.type = "number";
+            input.className = "matrixInput";
+            input.id = "matrixInput_" + i + "_" + j;
+            cell.appendChild(input);
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+
+    matrixTableDiv.appendChild(table);
+}
+
+function getMatrixFromTable(size) {
+    var matrix = [];
+    for (var i = 0; i < size; i++) {
+        var row = [];
+        for (var j = 0; j < size; j++) {
+            var inputValue = parseFloat(document.getElementById("matrixInput_" + i + "_" + j).value);
+            row.push(inputValue);
+        }
+        matrix.push(row);
+    }
+    return matrix;
+}
+
+function calculateDeterminant() {
+    var size = parseInt(document.getElementById("sizeInput").value);
+    var matrix = getMatrixFromTable(size);
+
+
+    var determinant = getDeterminant(matrix);
+    document.getElementById("result").innerHTML = "<p>Determinant: " + determinant + "</p>";
+}
+
+function getDeterminant(matrix) {
+    var n = matrix.length;
+    if (n === 1) {
+        return matrix[0][0];
+    } else if (n === 2) {
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+    }
+
+    var det = 0;
+    for (var j = 0; j < n; j++) {
+        var minor = [];
+        for (var k = 1; k < n; k++) {
+            minor.push(matrix[k].slice(0, j).concat(matrix[k].slice(j + 1)));
+        }
+        det += matrix[0][j] * Math.pow(-1, j) * getDeterminant(minor);
+    }
+
+    return det;
+}
