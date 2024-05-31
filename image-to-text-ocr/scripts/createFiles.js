4562108649002ef2636b92d145582887f4cf48b4
Nikunj <mnikunj262@gmail.com>
2024-05-31 22:09:32 +0530

    delete old stuff

--- image-to-text-ocr/scripts/createFiles.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const { locales } = require("./translate");
-const jsonfile = require("jsonfile");
-const parentPath = path.join(__dirname, "../src/app/_locales/");
-locales.map((loc) => {
-  const _path = path.join(parentPath, `/${loc}/messages.json`);
-  const data = {};
-  jsonfile.writeFileSync(_path, data, { flag: "w" });
-});
