3c6ff9e8f2e813febb6374925363a38d5539d6b9
Yashgabani845 <gabaniyash845@gmail.com>
2024-06-06 11:48:56 +0530

    css inspector

---
+++ CSS Inspector/popup.js
@@ -0,0 +1,9 @@
+document.getElementById('open-inspector').addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      chrome.scripting.executeScript({
+        target: {tabId: tabs[0].id},
+        files: ['content.js']
+      });
+    });
+  });
+  
