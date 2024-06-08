8e87ef8ec601a1533b560517a73f953dcc73a5b8
Anshika Yadav <14anshika7yadav@gmail.com>
2024-06-08 19:59:16 +0530

    Added Logarithm Calculator Extension: #1180

---
+++ Logarithm Calculator Extension/script.js
@@ -0,0 +1,21 @@
+document.getElementById('myButton').addEventListener('click', function () {
+    const number = parseFloat(document.getElementById('number').value);
+    const base = parseFloat(document.getElementById('base').value);
+
+    if (!isNaN(number) && !isNaN(base) && number > 0 && base > 0 && base !== 1) {
+        const result = Math.log(number) / Math.log(base);
+        document.getElementById('result').innerText = `Log base ${base} of ${number} is ${result.toFixed(4)}`;
+    }
+    else {
+        alert('Enter valid input');
+    }
+}
+)
+function validateBaseInput(base) {
+    const errorElement = document.getElementById('baseError');
+    if (base.value == 1) {
+        errorElement.style.display = 'inline';
+    } else {
+        errorElement.style.display = 'none';
+    }
+}
