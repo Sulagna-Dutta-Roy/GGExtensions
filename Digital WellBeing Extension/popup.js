0e56c2dce623583810b3a250c4d056ae4edbc909
Harshit_Shadow <hmharsh123@gmail.com>
2024-06-09 23:27:15 +0530

    Update2

--- Digital WellBeing Extension/popup.js
+++ Digital WellBeing Extension/popup.js
@@ -2,21 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const dataElement = document.getElementById('data');
     const resetButton = document.getElementById('reset');
 
-    // Function to display the data
     const displayData = () => {
         chrome.storage.local.get(null, data => {
             dataElement.innerHTML = '';
             for (const [url, time] of Object.entries(data)) {
                 const timeInMinutes = (time / 1000 / 60).toFixed(2);
-                dataElement.innerHTML += `<p>${url}: ${timeInMinutes} minutes</p>`;
+                dataElement.innerHTML += `
+            <div class="site">
+              <p>${url}</p>
+              <p>${timeInMinutes} min</p>
+            </div>`;
             }
         });
     };
 
-    // Initial display
     displayData();
 
-    // Reset button functionality
     resetButton.addEventListener('click', () => {
         chrome.storage.local.clear(() => {
             dataElement.innerHTML = '';
