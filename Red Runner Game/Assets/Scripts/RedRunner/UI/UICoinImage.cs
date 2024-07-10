59b8383e4f65f17869be2c5110016c3e5c6c17e3
Nikhil Raj <129590267+TheNikhilRaj@users.noreply.github.com>
2024-07-10 11:50:19 +0530

    Add files via upload

---
+++ Red Runner Game/Assets/Scripts/RedRunner/UI/UICoinImage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using RedRunner.Collectables;
+
+namespace RedRunner.UI
+{
+
+	public class UICoinImage : Image
+	{
+
+		[SerializeField]
+		protected ParticleSystem m_ParticleSystem;
+
+		protected override void Awake ()
+		{
+			base.Awake ();
+		}
+
+        protected override void Start()
+        {
+            GameManager.Singleton.m_Coin.AddEventAndFire(Coin_OnCoinCollected, this);
+        }
+
+        void Coin_OnCoinCollected (int coinValue)
+		{
+			GetComponent<Animator> ().SetTrigger ("Collect");
+		}
+
+		public virtual void PlayParticleSystem ()
+		{
+			m_ParticleSystem.Play ();
+		}
+	}
+}
---
+++ Red Runner Game/Assets/Scripts/RedRunner/UI/UICoinText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RedRunner.Collectables;
+using System;
+
+namespace RedRunner.UI
+{
+	public class UICoinText : UIText
+	{
+		[SerializeField]
+		protected string m_CoinTextFormat = "x {0}";
+
+		protected override void Awake ()
+		{
+			base.Awake ();
+		}
+
+        protected override void Start()
+        {
+            GameManager.Singleton.m_Coin.AddEventAndFire(UpdateCoinsText, this);
+        }
+
+        private void UpdateCoinsText(int newCoinValue)
+        {
+            GetComponent<Animator>().SetTrigger("Collect");
+            text = string.Format(m_CoinTextFormat, newCoinValue);
+        }
+	}
+}
