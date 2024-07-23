733f32d025a7e2c17ffbfc8b4b609d7689c8212d
Archiesachin <archieshah8767@gmail.com>
2024-07-23 14:21:36 +0530

    Scientific Calculator added

---
+++ Scientific Calculator/script.js
@@ -0,0 +1,23 @@
+function appendToDisplay(value) {
+  const display = document.getElementById('display');
+  display.value += value;
+}
+
+function clearDisplay() {
+  const display = document.getElementById('display');
+  display.value = '';
+}
+
+function backspace() {
+  const display = document.getElementById('display');
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  const display = document.getElementById('display');
+  try {
+      display.value = eval(display.value);
+  } catch (e) {
+      alert('Invalid expression');
+  }
+}
