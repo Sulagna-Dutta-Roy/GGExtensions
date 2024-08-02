707369ac98d26e8c560e6ca7dd3e84c40a35836e
P-lavanya16 <lavanyapuvvala16@gmail.com>
2024-08-02 16:11:08 +0530

    Added levels to generator

--- Capcha Generator/scripts/script.js
+++ Capcha Generator/scripts/script.js
@@ -3,14 +3,30 @@ const captchaTextBox = document.querySelector(".captch_box input");
 const refreshButton = document.querySelector(".refresh_button");
 const captchaInputBox = document.querySelector(".captch_input input");
 const message = document.querySelector(".message");
-const submitButton = document.querySelector(".button");
+const submitButton = document.querySelector(".button button");
+const difficultySelect = document.querySelector("#difficulty");
 
 // Variable to store generated captcha
 let captchaText = null;
 
 // Function to generate captcha
 const generateCaptcha = () => {
-  const randomString = Math.random().toString(36).substring(2, 7);
+  let captchaLength;
+
+  switch (difficultySelect.value) {
+    case "medium":
+      captchaLength = 7;
+      break;
+    case "hard":
+      captchaLength = 10;
+      break;
+    case "easy":
+    default:
+      captchaLength = 5;
+      break;
+  }
+
+  const randomString = Math.random().toString(36).substring(2, 2 + captchaLength);
   const randomStringArray = randomString.split("");
   const changeString = randomStringArray.map((char) => (Math.random() > 0.5 ? char.toUpperCase() : char));
   captchaText = changeString.join("   ");
@@ -25,13 +41,15 @@ const refreshBtnClick = () => {
 };
 
 const captchaKeyUpValidate = () => {
+  const isInputEmpty = !captchaInputBox.value.trim();
+  submitButton.parentElement.classList.toggle("disabled", isInputEmpty);
 
-  submitButton.classList.toggle("disabled", !captchaInputBox.value);
-
-  if (!captchaInputBox.value) message.classList.remove("active");
+  if (isInputEmpty) {
+    message.classList.remove("active");
+  }
 };
 
-//validation
+// Validation
 const submitBtnClick = () => {
   captchaText = captchaText
     .split("")
@@ -40,11 +58,11 @@ const submitBtnClick = () => {
   message.classList.add("active");
   // Check if the entered captcha text is correct or not
   if (captchaInputBox.value === captchaText) {
-    swal("Verification Success..","Hooray!","success");
+    swal("Verification Success..", "Hooray!", "success");
     message.innerText = "Entered captcha is correct";
     message.style.color = "#1940a5";
   } else {
-    swal("Incorrect!","Please enter the correct captcha.","warning")
+    swal("Incorrect!", "Please enter the correct captcha.", "warning");
     message.innerText = "Entered captcha is not correct";
     message.style.color = "#FF2525";
   }
@@ -54,6 +72,7 @@ const submitBtnClick = () => {
 refreshButton.addEventListener("click", refreshBtnClick);
 captchaInputBox.addEventListener("keyup", captchaKeyUpValidate);
 submitButton.addEventListener("click", submitBtnClick);
+difficultySelect.addEventListener("change", generateCaptcha);
 
 // Generate a captcha when the page loads
 generateCaptcha();
